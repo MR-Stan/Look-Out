cbb1983e6f4baf02aa7990942de5cc540ec813fb
MR-Stan <michaelrstanley87@gmail.com>
2019-12-19 21:14:13 -0600

    added database folder, sql files, created schema

---
+++ models/database/schema.sql
@@ -0,0 +1,45 @@
+-- delete the lookout_db database if it already exists
+DROP DATABASE IF EXISTS lookout_db;
+
+-- create a new database named lookout_db
+CREATE DATABASE lookout_db;
+
+-- use the lookout_db database
+USE lookout_db;
+
+-- create a table named user_date to store the user specific data
+CREATE TABLE user_data
+(
+    id INT(255)
+    AUTO_INCREMENT NOT NULL,
+    -- user's first name
+    firstname VARCHAR
+    (255),
+    -- user's last name
+    lastname VARCHAR
+    (255),
+    -- username
+    username VARCHAR
+    (255),
+    -- hashed password
+    hashedpw VARCHAR
+    (255),
+    -- user's email address
+    email VARCHAR
+    (255),
+    -- default location saved each time a user logs in
+    defaultlocation VARCHAR
+    (255),
+    -- user's favorites
+    favorites VARCHAR
+    (255),
+    -- date and time the user was created
+    created DATETIME,
+    -- date and time the user's information was last updated
+    updated DATETIME,
+    -- date and time the user last logged in
+    lastlogin DATETIME,
+    PRIMARY KEY
+    (id)
+);
+
